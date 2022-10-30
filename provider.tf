80fc2751b1d524cb1a5870e6365c1d8dc965a8d8
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-10-30 19:52:08 +0500

    Adding provider

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "~> 1.2.7"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.34.0"
+    }
+  }
+}
+
+provider "google" {
+    project     = var.project
+    region      = var.region
+    zone        = var.zone
+    credentials = file("credentials/credentials.json")
+  
+}
