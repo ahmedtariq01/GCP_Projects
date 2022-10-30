a1576097136c58cd2aea0d0eb53b9dbad3801f7b
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-10-30 19:54:18 +0500

    Adding variables file

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  description = "value of Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "value of zone"
+  type        = string
+  default     = "us-central1-a"
+}
+variable "project" {
+  description = "value of project"
+  type        = string
+  default     = "project name"
+}
